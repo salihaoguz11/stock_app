f553fd5b757b7d7aab8c70712fb65ae143907eb6
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-03-27 13:20:02 +0100

    global style

--- stock-app/src/pages/Firms.jsx
+++ stock-app/src/pages/Firms.jsx
@@ -2,17 +2,29 @@ import Typography from "@mui/material/Typography";
 import { Button, Grid } from "@mui/material";
 import { useEffect } from "react";
 import useStockCall from "../hooks/useStockCall";
+import { useSelector } from "react-redux";
+import FirmCard from "../components/FirmCard";
+import { flex } from "../styles/globalStyle";
 const Firms = () => {
   const { getStockData } = useStockCall();
+  const { firms } = useSelector((state) => state.stock);
   useEffect(() => {
     getStockData("firms");
   }, []);
+  console.log(firms);
   return (
     <div>
       <Typography variant="h4" color="error" mb={3}>
         Firm
       </Typography>
       <Button variant="contained">New Firm</Button>
+      <Grid container sx={flex}>
+        {firms?.map((firm) => (
+          <Grid item key={firm.id}>
+            <FirmCard firm={firm} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
