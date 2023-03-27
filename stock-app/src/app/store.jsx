fe2f3921e0f2e1036e9ebf1c4abf5233b03a6339
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-03-27 09:47:29 +0100

    stockreducer

--- stock-app/src/app/store.jsx
+++ stock-app/src/app/store.jsx
@@ -1,5 +1,6 @@
 import { configureStore, createStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import stockReducer from "../features/stockSlice";
 
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
@@ -14,6 +15,7 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
+    stock: stockReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
