f1018e7ac6d46f36d358a3495b698e474eb0171a
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-04-01 23:38:34 +0100

    Home compenent

--- stock-app/src/app/store.jsx
+++ stock-app/src/app/store.jsx
@@ -1,4 +1,4 @@
-import { configureStore, createStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import stockReducer from "../features/stockSlice";
 
