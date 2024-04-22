3d75ddb35b9249e9a8ad3003f19f1082bbd7c5c2
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-04-22 16:06:58 +0100

    update

--- stock-app/src/hooks/useAuthCall.jsx
+++ stock-app/src/hooks/useAuthCall.jsx
@@ -14,7 +14,7 @@ import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 const useAuthCall = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const BASE_URL = "https://12131.fullstack.clarusway.com/";
+  const BASE_URL = "https://stockapi-server.onrender.com";
   const login = async (userInfo) => {
     dispatch(fetchStart());
     try {
