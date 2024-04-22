3626f748b30e669c11ae8bce3671323a7534c219
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-04-22 17:03:59 +0100

    up

--- stock-app/src/router/PrivateRouter.jsx
+++ stock-app/src/router/PrivateRouter.jsx
@@ -2,7 +2,8 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRouter = () => {
-  const { currentUser } = useSelector((state) => state.auth);
+  // const { currentUser } = useSelector((state) => state.auth);
+  const currentUser = true;
 
   return currentUser ? <Outlet /> : <Navigate to="/" />;
 };
